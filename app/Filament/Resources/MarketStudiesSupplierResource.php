cf8492dac972836fa2ff0dc845870009c5701df3
Cedric Athelstan <camparungao2021@plm.edu.ph>
2024-06-10 22:57:48 +0800

    line 35

--- app/Filament/Resources/MarketStudiesSupplierResource.php
+++ app/Filament/Resources/MarketStudiesSupplierResource.php
@@ -32,7 +32,7 @@ class MarketStudiesSupplierResource extends Resource
 
     protected static ?string $navigationGroup = 'MARKETSTUDY MANAGEMENT (in-house)';
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 3;
 
     public static function updateTotal($get, $set):void
     {
